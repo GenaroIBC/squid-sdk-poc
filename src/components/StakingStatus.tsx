c0ac3e50692c4c017a42584b6b097f954f8aeef3
genaroibc <bonavitaismael@gmail.com>
2023-05-23 09:58:00 -0300

    [FEAT]: change staking status design

--- src/components/StakingStatus.tsx
+++ src/components/StakingStatus.tsx
@@ -3,38 +3,42 @@ import { StatusResponse } from "@0xsquid/sdk"
 type Props = {
   status?: StatusResponse | null
 }
+
 export function StakingStatus({ status }: Props) {
   return (
     <>
       {status ? (
         <article className="flex flex-col gap-4 p-4 bg-slate-800 rounded-md my-4">
-          <p className="text-green-400 font-bold">
-            Success!{" "}
-            <a href={status.axelarTransactionUrl} target="_blank">
+          <header className="flex items-center gap-2 justify-between">
+            <span className="text-green-400">Success!</span>
+
+            <a
+              href={status.axelarTransactionUrl}
+              target="_blank"
+              className="text-sm"
+            >
               View on Axelar scan
             </a>
-          </p>
+          </header>
 
           <div className="flex items-center gap-4">
             <span className="flex items-center gap-2">
-              From
+              From {status.fromChain?.chainData.chainName}
               <img
-                width={32}
-                height={32}
+                width={20}
+                height={20}
                 src={status.fromChain?.chainData.chainIconURI}
                 alt={status.fromChain?.chainData.chainName}
               />
-              {status.fromChain?.chainData.chainName}
             </span>
             <span className="flex items-center gap-2">
-              to
+              to {status.toChain?.chainData.chainName}
               <img
-                width={32}
-                height={32}
+                width={20}
+                height={20}
                 src={status.toChain?.chainData.chainIconURI}
                 alt={status.toChain?.chainData.chainName}
               />
-              {status.toChain?.chainData.chainName}
             </span>
           </div>
         </article>
